6b5b43cd0ecdb668ca3030825cf1930bc76c7bfc
OlegK <oleg2kv@gmail.com>
2021-12-10 19:23:57 +0200

    Detached elements

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,16 +4,12 @@
   <div style="display: flex;">
     <div v-for="s in steps" class="" :key="s.id">
       <div>{{ s.id }}</div>
-      <div>
-        <div v-for="item in getList(s.id)" :key="item.id" style="border: 1px solid gray; margin: 2px;">
-          <div>name: {{item.name}}</div>
-          <div>title: {{item.title}}</div>
-          <div>element with dynamic value - {{ item.id }}</div>
-          <div :key="item.id">element with key</div>
+        <div v-for="(a) in getList(s.id)" :key="a.id" style="border: 1px solid gray; margin: 2px;">
+          <div>element with dynamic value - {{ a.id }}</div>
+          <div :key="a.id">element with key</div>
           <div>element with static text</div>
         </div>
       </div>
-    </div>
   </div>
 </template>
 
@@ -31,16 +27,11 @@ export default defineComponent({
     const c = ref(1);
 
     const getList = function (a:string) {
-      let _a: {
-        id: string,
-        name:string,
-        title: string
-      }[] = [];
+      let _a: {id: string, name:string}[] = [];
       for (let i = 0; i < 3000; i++) {
         _a.push({
           id: 'item_id_' + a + i,
-          name: 'item_name_' + a + i,
-          title: 'item_title_' + a + i
+          name: 'item_name_' + a + i
         })
       }
       return _a;
